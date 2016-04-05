f83a038f9279f73158eb49b228b7b254a35cc2e5
ErichClark <erich.clark@gmail.com>
2016-04-05 18:59:17 -0400

    fixed git? Added more better battleship logic, fixed new fonts and new graphics

--- Better Battleship/battleship.js
+++ Better Battleship/battleship.js
@@ -1,3 +1,49 @@
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	ships = 
+		[{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+		{ locations: ["32", "33", "34"], hits: ["", "", ""] },
+		{ locations: ["63", "64", "65"], hits: ["", "", "hit"] }],
+	
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var.ships = this.ships[1];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				return true;
+			}
+		}
+		return false;
+	}
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
+/*
 function parseGuess(guess) {
 	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
@@ -19,3 +65,12 @@ function parseGuess(guess) {
 	}
 	return null;
 }
+*/
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Tap tap, is this thing on?");
